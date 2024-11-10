eaf9098ce27feff9327452e3576a1241d2c4f17a
Toufic_AlMabsout <tia09@mail.aub.edu>
2024-11-11 01:18:41 +0200

    changing test_app.py to work on assertion and changed jenkins for part 4

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import unittest
 from app import greet
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World"), "Hello, World!")
 
 
 if __name__ == "__main__":
